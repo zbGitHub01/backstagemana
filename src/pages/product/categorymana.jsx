90495ef0e2e84ee21a0ace97fc9e164baa71b313
unknown <zb.zhu@sky-mobi.com>
2021-06-09 17:53:10 +0800

    third commit

--- src/pages/product/categorymana.jsx
+++ src/pages/product/categorymana.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Button, Card,message,Table} from 'antd'
-import {PlusSquareOutlined} from '@ant-design/icons'
+import {PlusSquareOutlined,SendOutlined} from '@ant-design/icons'
 import LinkButton from '../../components/link-button'
 import { reqCategorys } from '../../api'
 
@@ -8,7 +8,10 @@ export default class Categorymana extends Component {
 
     state = {
         categorys: [], //一级分类列表
-        loading :false
+        loading :false,
+        parentId : '0', //当前需要显示的 分类列表ID
+        parentName : '', 
+        childCategorys : []
     }
 
     /*
@@ -25,15 +28,43 @@ export default class Categorymana extends Component {
             {
               title: '操作',
               width:500,
-              render: () => (
+              render: (category) => (
                   <span>
                       <LinkButton>修改分类</LinkButton>
-                      <LinkButton>查看子分类</LinkButton>
+                      {
+                          //防止一渲染就调用
+                      }
+
+                    {this.state.parentId ==='0'? <LinkButton 
+                    onClick={() => {this.showChildCategorys(category)}}>查看子分类
+                    </LinkButton>:null}
+                      
                   </span>
+
               )
             }
            
           ];
+     }
+
+     /**
+      * 显示指定一级分类对象的二级列表 
+      */
+     showChildCategorys = (category) => {
+         this.setState({
+            parentId : category._id,
+            parentName: category.name
+        },()=>{
+            this.getCategorys()
+        })
+     }
+
+     showCategorys = () => {
+        this.setState({
+            parentId : '0',
+            parentName: '',
+            childCategorys: []
+        })
      }
 
      /**
@@ -42,22 +73,32 @@ export default class Categorymana extends Component {
      getCategorys = async() => {
         //发送请求前 显示loading
         this.setState({loading: true})
-
+        const {parentId} = this.state
         //发异步ajax请求 获取数据
-        const result = await reqCategorys('0')
+        const result = await reqCategorys(parentId)
 
         //请求完成 隐藏 loading
         this.setState({loading:false})
 
         if(result.status === 0){
+            //取出分类数组（可能一级也可能二级）
             const categorys = result.data
+            if(parentId ==='0'){
+                this.setState({categorys})
+                console.log(categorys)
+            }else {
+                this.setState ({
+                    childCategorys : categorys
+                })
+            }
             
-            this.setState({categorys})
-            console.log(categorys)
+           
         }else {
             message.error('获取失败')
         }
      }
+
+
 
 
      /**
@@ -76,9 +117,14 @@ export default class Categorymana extends Component {
     }
 
     render() {
-        const {categorys,loading} = this.state
+        const {categorys,loading,parentId,childCategorys,parentName} = this.state
          //card左侧
-         const title = '一级分类列表'
+         
+         const title = parentId==='0'? '一级分类列表' : <span>
+             <LinkButton onClick={() =>{this.showCategorys()}}>一级分类列表</LinkButton>
+             <SendOutlined style={{ fontSize: '16px', color: '#08c' ,marginRight: '20px' }}/>
+             <span>{parentName} </span>
+         </span>
 
          //card右侧
          const extra = (
@@ -91,7 +137,7 @@ export default class Categorymana extends Component {
         return (
             <Card title={title} extra={<a href="#">{extra}</a>} style={{ width: '100%' }}>
                 <Table 
-                dataSource={categorys} 
+                dataSource={parentId=='0'? categorys : childCategorys} 
                 columns={this.columns} 
                 bordered 
                 rowKey='_id'
